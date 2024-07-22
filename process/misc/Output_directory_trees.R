6f899f59b553fad920aaad22eca10d7f1360d95e
Shoaib Ajaib <ajxa@pm.me>
2024-07-22 17:21:32 +0100

    changed gitignore to add misc dir in the process scripts

---
+++ process/misc/Output_directory_trees.R
@@ -0,0 +1,13 @@
+roots = list()
+directory_trees = list()
+
+# IMAGE VISUALISATION ----------------------------------------------------------
+roots$image_visualisation = "outputs/image_visualisation"
+
+directory_trees$image_visualisation = list(
+    
+    "paper_2" = list()
+
+)
+
+# END --------------------------------------------------------------------------
